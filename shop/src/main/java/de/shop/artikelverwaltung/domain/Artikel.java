d8f7f4e77938f46d862d8db92ce64b865f79e272
Christian Weick <wech1015(at)HS-Karlsruhe.de>
2013-05-16 11:22:36 +0200

    Validierung-Messages erneuert

--- shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
+++ shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -34,9 +34,11 @@ public class Artikel implements Serializable {
 	private String farbe;
 	
 	@NotNull(message = "{artikelverwaltung.artikel.preiskunde.notNull}")
+	@Min(value = 0, message = "{artikelverwaltung.artikel.preiskunde.min}")
 	private BigDecimal preisKunde;
 	
 	@NotNull(message = "{artikelverwaltung.artikel.preislieferant.notNull}")
+	@Min(value = 0, message = "{artikelverwaltung.artikel.preislieferant.min}")
 	private BigDecimal preisLieferant;
 	
 	@NotNull(message = "{artikelverwaltung.artikel.bestand.notNull}")
