652a9ac16e4a18f6127086c99b63214ab2bfdd03
Calvin Paciner <paca1011@hs-karlsruhe.de>
2013-11-06 16:11:27 +0100

    Kunde Test

--- shop/src/test/java/de/shop/kundenverwaltung/rest/KundeResourceTest.java
+++ shop/src/test/java/de/shop/kundenverwaltung/rest/KundeResourceTest.java
@@ -1,5 +1,6 @@
 package de.shop.kundenverwaltung.rest;
 
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -29,6 +30,8 @@ import de.shop.util.AbstractResourceTest;
 @RunWith(Arquillian.class)
 public class KundeResourceTest extends AbstractResourceTest {
 	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+	
+	private static final String NACHNAME_VORHANDEN = "Admin";
 	
 	@Test
 	@InSequence(1)
@@ -71,6 +74,58 @@ public class KundeResourceTest extends AbstractResourceTest {
 		assertThat(response.getStatus()).isEqualTo(HTTP_OK);
 		Kunde kunde = response.readEntity(Kunde.class);
 		assertThat(kunde.getId()).isEqualTo(kundeId);		
+		
+		LOGGER.finer("ENDE");
+	}
+	
+	@Test
+	@InSequence(4)
+	@Ignore
+	public void findKundeByIdNichtVorhanden() {
+		LOGGER.finer("BEGINN");
+		
+		// Given
+		final Long kundeId = Long.valueOf(333);
+		
+		// When
+		Response response = ClientBuilder.newClient()
+									.target("http://localhost:8080/shop/rest/kunden/{id}")
+									.resolveTemplate("id", kundeId)
+									.request()
+									.accept(APPLICATION_JSON)
+									.acceptLanguage(Locale.GERMAN)
+									.get();
+	
+		// Then
+		assertThat(response.getStatus()).isEqualTo(HTTP_NOT_FOUND);
+		Kunde kunde = response.readEntity(Kunde.class);
+		assertThat(kunde.getId()).isEqualTo(kundeId);		
+		
+		LOGGER.finer("ENDE");
+	}
+	
+	@Test
+	@InSequence(5)
+	@Ignore
+	public void findKundeByNachname() {
+		LOGGER.finer("BEGINN");
+		
+		// Given
+		final String kundeNachname = NACHNAME_VORHANDEN;
+		
+		// When
+		Response response = ClientBuilder.newClient()
+									.target("http://localhost:8080/shop/rest/kunden/prefix/nachname/{nachname}")
+									.resolveTemplate("nachname", kundeNachname)
+									.request()
+									.accept(APPLICATION_JSON)
+									.acceptLanguage(Locale.GERMAN)
+									.get();
+	
+		// Then
+		assertThat(response.getStatus()).isEqualTo(HTTP_OK);
+		Kunde kunde = response.readEntity(Kunde.class);
+		assertThat(kunde.getNachname()).isEqualTo(kundeNachname);		
 		
 		LOGGER.finer("ENDE");
 	}
